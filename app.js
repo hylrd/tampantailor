94f650efde7ceea9d8808fe96f5b59e81e795c16
haekal <haekal.yudh@gmail.com>
2019-12-20 00:17:32 +0700

    add to cart

--- app.js
+++ app.js
@@ -4,10 +4,19 @@ const port = 3000;
 const bodyParser = require('body-parser')
 
 const UserRouter = require ("./routes/UserRouter");
+const session = require('express-session')
+
+app.use(session({
+  secret: 'bebas',
+  resave: false,
+  saveUninitialized: true,
+
+}))
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use('/user', UserRouter);
+
 
 app.listen(port, () => console.log("Listening At", port));
